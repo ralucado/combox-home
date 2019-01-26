827679fdd683d7d282c8dad4d3acb44074ca3930
Ralu <ralucris.v@gmail.com>
2019-01-26 20:24:27 +0100

    Upload project

---
+++ Combox Home/Assets/Scripts/Enemy1Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Controller : MonoBehaviour {
+	public float _movSpeed;
+	private Vector2 _movVel;
+
+	// Use this for initialization
+	void Start () {
+		_movSpeed = 20.0f;
+	}
+
+
+	void Update(){
+		if (transform.position.x <= -18.5) {
+			_movSpeed = Mathf.Abs (_movSpeed);
+		} else if (transform.position.x >= 18.5) {
+			_movSpeed = -Mathf.Abs (_movSpeed);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate (new Vector3(_movSpeed * Time.fixedDeltaTime, 0.0f, 0.0f));
+	}
+
+
+}
---
+++ Combox Home/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody2D _rigidbody;
+	private float _movSpeed;
+	private float _jumpSpeed;
+	private Vector2 _movVel;
+	public Vector2 _jmpVel;
+	private bool _startJump;
+	private bool _startDunk;
+	private bool _bufferedJump;
+	private float _timerJump;
+	private float _timerDunk;
+	private bool _bufferedDunk;
+
+
+	private BoxCollider2D _lastFloorCollider;
+	public LayerMask mask;
+	public float _maxBufferTime;
+
+	// Use this for initialization
+	void Start () {
+		_rigidbody = GetComponent<Rigidbody2D> ();
+		_movSpeed = 40.0f;
+		_jumpSpeed = 150.0f;
+		_timerJump = 0.0f;
+		_timerDunk = 0.0f;
+		_startJump = false;
+		_startDunk = false;
+		_bufferedJump = false;
+		_bufferedDunk = false;
+		_maxBufferTime = 0.2f;
+	}
+
+	void Update(){
+		_timerJump = _timerJump+Time.deltaTime;
+		_timerDunk = _timerDunk+Time.deltaTime;
+		float mv_horizontal = Input.GetAxis ("Horizontal");
+		_movVel = (new Vector2 (-mv_horizontal, 0.0f).normalized * _movSpeed);
+		if (isGrounded () && Input.GetKeyDown (KeyCode.UpArrow)) {
+			_startJump = true; 
+		} else if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			_bufferedJump = true;
+			_timerJump = 0.0f;
+		} else {
+			if (isGrounded() && _lastFloorCollider != null && !_lastFloorCollider.enabled) {
+				_lastFloorCollider.enabled = true;
+			}
+			if (isGrounded () && _bufferedJump && _timerJump < _maxBufferTime) {
+				_startJump = true;
+				_timerJump = 0.0f;
+				_bufferedJump = false;
+				Debug.Log ("Buffer Jumping");
+			} else {
+				_startJump = false;
+			}
+		}
+
+		if (canGoDown () && Input.GetKeyDown (KeyCode.DownArrow)) {
+			_startDunk = true;
+			_lastFloorCollider.enabled = false;
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			_bufferedDunk = true;
+			_timerDunk = 0.0f;
+		} else {
+			if (canGoDown() && _bufferedDunk && _timerDunk < _maxBufferTime) {
+				_startDunk = true;
+				_lastFloorCollider.enabled = false;
+				_timerDunk = 0.0f;
+				_bufferedDunk = false;
+				Debug.Log ("Buffer Dunking");
+			} else {
+				_startDunk = false;
+			}
+		}
+
+		if (_startJump) {
+			_jmpVel = new Vector2 (0.0f, _jumpSpeed);
+		} else if (_startDunk) {
+			_jmpVel = new Vector2 (0.0f, -(_movSpeed)); 
+		} else if (!isGrounded ()) {
+			//_jmpVel.y = Mathf.Max (0.0f, _jmpVel.y - (_jumpSpeed / 8.0f));
+			_jmpVel.y = _jmpVel.y - (_jumpSpeed / 8.0f);
+
+		}
+
+
+	}
+
+	void FixedUpdate(){
+		_rigidbody.MovePosition(_rigidbody.position + (_movVel + _jmpVel) * Time.fixedDeltaTime );
+		//Debug.DrawLine (transform.position, transform.position + (Vector2.down * 0.1f));
+	}
+
+	bool canGoDown(){
+		RaycastHit2D hit = Physics2D.Raycast (transform.position, Vector2.down, GetComponent<BoxCollider2D> ().bounds.extents.y + 0.1f, mask);
+		if (hit && hit.collider.gameObject.CompareTag ("DownGround")){
+			_lastFloorCollider = hit.collider.GetComponent<BoxCollider2D>();
+			return true;
+		}
+		return false;
+	}
+
+	bool isGrounded(){			
+		return Physics2D.Raycast (transform.position, Vector2.down, GetComponent<BoxCollider2D>().bounds.extents.y+0.1f, mask);
+	}
+
+
+
+}
