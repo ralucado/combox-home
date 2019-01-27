2c795641887db16eae4b49729a091815b754663a
Ralu <ralucris.v@gmail.com>
2019-01-27 14:50:28 +0100

    Main Menu

---
+++ Combox Home/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneHandler : MonoBehaviour {
+
+	public void LoadMain(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void loadInGame(){
+		SceneManager.LoadScene ("InGame");
+	}
+}
